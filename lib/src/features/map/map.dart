3d091bf68878e087261821ef97d2b0da969342a5
Dudlileif <dudlileif@gmail.com>
2023-05-08 20:59:06 +0200

    Add copyWith extensions on map features

--- lib/src/features/map/map.dart
+++ lib/src/features/map/map.dart
@@ -1,5 +1,5 @@
 export 'models/models.dart';
 export 'providers/providers.dart';
 export 'screens/map_view.dart';
-export 'utils/lat_lng_tween.dart';
+export 'utils/utils.dart';
 export 'widgets/map_widgets.dart';
---
+++ lib/src/features/map/utils/utils.dart
@@ -0,0 +1,2 @@
+export 'lat_lng_tween.dart';
+export 'map_features_extensions.dart';
