7471cb34b957e9669731c00cb7b60dc194d04a84
Dudlileif <dudlileif@gmail.com>
2023-07-28 22:49:20 +0200

    Add bearing difference functions and tests

--- lib/src/features/common/utils/math.dart
+++ lib/src/features/common/utils/math.dart
@@ -2,3 +2,23 @@ import 'dart:math';
 
 /// Find the [value] modulo 2π.
 double mod2pi(double value) => value % (2 * pi);
+
+/// The absolute minimal difference in degrees between [bearing1] and
+/// [bearing2].
+double bearingDifference(double bearing1, double bearing2) =>
+    180 - (180 - ((bearing2 - bearing1) % 360)).abs();
+
+/// The signed minimal difference in degrees from [bearing1] to [bearing2].
+double signedBearingDifference(double bearing1, double bearing2) {
+  final difference = (bearing2 - bearing1) % 360;
+
+  if (difference < -180) {
+    return difference + 360;
+  }
+
+  if (difference >= 180) {
+    return difference - 360;
+  }
+
+  return difference;
+}
