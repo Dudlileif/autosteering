aff29770476e2930df23c90b0cc06e3c8302b410
Dudlileif <dudlileif@gmail.com>
2023-08-13 20:55:24 +0200

    Add wakelock to keep screen always on

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:agopengps_flutter/src/features/common/common.dart';
 import 'package:fast_cached_network_image/fast_cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 import 'package:window_manager/window_manager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await WakelockPlus.enable();
 
   if (Device.isDesktop) {
     await windowManager.ensureInitialized();
